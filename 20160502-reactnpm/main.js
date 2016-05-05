732c11c955408be01b7d6eddb9f2cb5c3710fde4
yassh <yassh32@outlook.jp>
2016-05-05 22:30:52 +0900

    var -> const

--- 20160502-reactnpm/main.js
+++ 20160502-reactnpm/main.js
@@ -1,5 +1,5 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
