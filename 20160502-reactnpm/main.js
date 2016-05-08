eb3b4d5c5902628e7d7d36ab5001ec084464a7ca
yassh <yassh32@outlook.jp>
2016-05-09 08:42:09 +0900

    update 20160502-reactnpm

--- 20160502-reactnpm/main.js
+++ 20160502-reactnpm/main.js
@@ -5,3 +5,16 @@ ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('example')
 );
+
+class SampleComponent extends React.Component {
+  render() {
+    return (
+      <div>SampleComponent</div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <SampleComponent/>,
+  document.getElementById('sample')
+);
