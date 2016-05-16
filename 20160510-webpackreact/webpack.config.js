37060725d44ae6344e7e81fc2b0775bd380ddb8a
yassh <yassh32@outlook.jp>
2016-05-16 09:29:49 +0900

    update 20160510-webpackreact

--- 20160510-webpackreact/webpack.config.js
+++ 20160510-webpackreact/webpack.config.js
@@ -1,4 +1,14 @@
+// http://webpack.github.io/docs/configuration.html
+
 module.exports = {
+  context: __dirname + '/src',
+  entry: {
+    index: './index.jsx'
+  },
+  output: {
+    path: __dirname + "/built",
+    filename: "[name].js"
+  },
   module: {
     loaders: [
       { test: /\.jsx?$/, loader: "babel-loader" },
