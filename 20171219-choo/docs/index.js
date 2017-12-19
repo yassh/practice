915901da90b0e2743897ab7d91c85d1e29e6ade9
yassh <yassh32@outlook.jp>
2017-12-19 13:34:13 +0900

    update prettier config

--- 20171219-choo/docs/index.js
+++ 20171219-choo/docs/index.js
@@ -1,5 +1,5 @@
-import html from 'choo/html';
-import choo from 'choo';
+import html from 'choo/html'
+import choo from 'choo'
 
 function mainView(state, emit) {
   return html`
@@ -7,22 +7,22 @@ function mainView(state, emit) {
       <h1>count is ${state.count}</h1>
       <button onclick=${onclick}>Increment</button>
     </body>
-  `;
+  `
 
   function onclick() {
-    emit('increment', 1);
+    emit('increment', 1)
   }
 }
 
 function countStore(state, emitter) {
-  state.count = 0;
+  state.count = 0
   emitter.on('increment', function(count) {
-    state.count += count;
-    emitter.emit('render');
-  });
+    state.count += count
+    emitter.emit('render')
+  })
 }
 
-const app = choo();
-app.use(countStore);
-app.route('/', mainView);
-app.mount('body');
+const app = choo()
+app.use(countStore)
+app.route('/', mainView)
+app.mount('body')
