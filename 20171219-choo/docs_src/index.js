47e874e9b54f38dd09b3d3c8964226cd426c48ff
yassh <yassh32@outlook.jp>
2017-12-21 11:23:40 +0900

    update 20171219-choo

--- 20171219-choo/docs_src/index.js
+++ 20171219-choo/docs_src/index.js
@@ -1,28 +1,64 @@
 import html from 'choo/html'
 import choo from 'choo'
 
-function mainView(state, emit) {
-  return html`
-    <body>
-      <h1>count is ${state.count}</h1>
-      <button onclick=${onclick}>Increment</button>
-    </body>
-  `
-
+function indexView(state, emit) {
   function onclick() {
     emit('increment', 1)
   }
+
+  return html`
+    <div>
+      <h1>Index</h1>
+      <div>
+        <button onclick=${onclick}>Increment</button>
+        ${state.count}
+      </div>
+      <div>☞ <a href="/foo">foo</a></div>
+    </div>
+`
+}
+
+function fooView(state, emit) {
+  function onclick() {
+    emit('increment', 1)
+  }
+
+  function oninput(e) {
+    emit('input', e.target.value)
+  }
+
+  return html`
+    <div>
+      <h1>Foo</h1>
+      <div>
+        <button onclick=${onclick}>Increment</button>
+        ${state.count}
+      </div>
+      <div>
+        <input type="text" value=${state.inputValue} oninput=${oninput} />
+        ${state.inputValue}
+      </div>
+    </div>
+  `
 }
 
 function countStore(state, emitter) {
   state.count = 0
-  emitter.on('increment', function(count) {
+  state.inputValue = ''
+
+  emitter.on('increment', (count) => {
     state.count += count
+    emitter.emit('render')
+  })
+
+  emitter.on('input', (inputValue) => {
+    state.inputValue = inputValue
     emitter.emit('render')
   })
 }
 
 const app = choo()
 app.use(countStore)
-app.route('/', mainView)
-app.mount('body')
+app.route('/', indexView)
+app.route('/foo', fooView)
+app.mount('#app')
