6bf867b3018e22ccfe7de14a265495d67bb8ac87
yassh <yassh32@outlook.jp>
2018-02-14 19:22:20 +0900

    update 20180214-lithtml

--- 20180214-lithtml/src/index.js
+++ 20180214-lithtml/src/index.js
@@ -2,4 +2,5 @@ import { html, render } from 'lit-html'
 
 const helloTemplate = (name) => html`<div>Hello ${name}!</div>`
 
-render(helloTemplate('Steve'), document.getElementById('app'))
+render(helloTemplate('Steve'), document.getElementById('hello1'))
+render(helloTemplate('真央'), document.getElementById('hello2'))
