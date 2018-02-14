0c4128913ba1d51f9adeddac97ce7bbcd93ddbb5
yassh <yassh32@outlook.jp>
2018-02-14 19:27:41 +0900

    update 20180214-lithtml

--- 20180214-lithtml/src/index.js
+++ 20180214-lithtml/src/index.js
@@ -4,3 +4,4 @@ const helloTemplate = (name) => html`<div>Hello ${name}!</div>`
 
 render(helloTemplate('Steve'), document.getElementById('hello1'))
 render(helloTemplate('真央'), document.getElementById('hello2'))
+render(helloTemplate('<u>&amp;</u>'), document.getElementById('hello3'))
